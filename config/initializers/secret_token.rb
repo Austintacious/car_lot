f3cda383959fe1a7194de51e37e34c963c14003d
Austin Winslow <awinslow21@gmail.com>
2014-01-02 18:26:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarLot::Application.config.secret_key_base = '8c26a711639af19851b8a299cfa92316fe6cab80cbcde7c62d4601e1536ebab8d17501ed091cc0de632a6a1172f19ed862f6c972b2918db25817a7527b11487e'
