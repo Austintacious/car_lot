8193f9415c600b01946329adb78bef2834d38d4d
Austin Winslow <awinslow21@gmail.com>
2014-01-02 21:45:47 -0500

    add new/show and tests

---
+++ spec/features/user_adds_a_car_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "User adds a new car" do
+
+  context "with valid attributes" do
+    it 'creates a new car' do
+      visit '/cars/new'
+      fill_in "Color", with: "Bloo"
+      fill_in "Year", with: 2012
+      fill_in "Milage", with: 525600
+      fill_in "Description", with: "She ain't beautiful, but she'll getcha there."
+      click_on "Create Car"
+      expect(page).to have_content("Bloo")
+    end
+  end
+
+  context "without valid attributes" do
+    it 'will not save a car without proper attributes' do
+      visit '/cars/new'
+      click_on "Create Car"
+      expect(page).to have_content("can't be blank")
+    end
+  end
+  
+end
