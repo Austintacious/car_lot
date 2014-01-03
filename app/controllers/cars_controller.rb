5b200ca815f0b226dad504ca020201074d91165b
Austin Winslow <awinslow21@gmail.com>
2014-01-02 19:59:47 -0500

    add cars_controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,37 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def show
+    set_car
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to car_path(@car),
+      notice: "Car successfully added!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    set_car
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end 
+
+  def car_params
+    params.require(:car).permit(:color, :year, :milage, :description)
+  end
 end
